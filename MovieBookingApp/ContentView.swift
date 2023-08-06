7cf33bd121bd77183d5d961ca06fcf349a10d73b
Elaidzha1940 <eli080293@gmail.com>
2023-08-06 08:53:38 +0300

    MovieBookingApp

---
+++ MovieBookingApp/ContentView.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: MovieBookingApp
+//  File: ContentView.swift
+//  Created by Elaidzha Shchukin
+//  Date: 06.08.2023
+//
+//  Status: In progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieBookingApp/MovieBookingAppApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: MovieBookingApp
+//  File: MovieBookingAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 06.08.2023
+//
+//  Status: In progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct MovieBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
