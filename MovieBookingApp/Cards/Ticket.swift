e8e19466f660ac01b22b15ebfddae78f64438910
Elaidzha1940 <eli080293@gmail.com>
2023-08-06 10:18:49 +0300

    Ticket

--- MovieBookingApp/Cards/Ticket.swift
+++ MovieBookingApp/Cards/Ticket.swift
@@ -13,25 +13,37 @@
 import SwiftUI
 
 struct Ticket: View {
+    
+    var gradient = [Color("cyan"), Color("cyan").opacity(0), Color("cyan").opacity(0)]
+    
     var body: some View {
         
         VStack(spacing: 0) {
             VStack(spacing: 4) {
                 
-                Text("Movie")
+                Text("Sherlok")
                     .font(.system(size: 20, weight: .semibold, design: .rounded))
                 
-                Text("Movie1")
+                Text("Sherlok")
                     .font(.system(size: 20, weight: .regular, design: .rounded))
             }
             .padding(EdgeInsets(top: 20, leading: 30, bottom: 0, trailing: 30))
             .frame(width: 250, height: 325, alignment: .top)
             .foregroundColor(.white)
             .background(
-            Image("sherlok")
+            Image("sherlok-top")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
             )
+            .mask (
+                Image("sherlok-top")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            )
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(LinearGradient(colors: <#T##[Color]#>, startPoint: <#T##UnitPoint#>, endPoint: <#T##UnitPoint#>))
+            }
         }
         .frame(width: 460)
         .shadow(radius: 10)
