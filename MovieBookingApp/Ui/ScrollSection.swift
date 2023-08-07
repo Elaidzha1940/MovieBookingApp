f137e62075408a869b562ea3e6debf595ad8bb0e
Elaidzha1940 <eli080293@gmail.com>
2023-08-07 17:11:36 +0300

    added some more Images

--- MovieBookingApp/Ui/ScrollSection.swift
+++ MovieBookingApp/Ui/ScrollSection.swift
@@ -12,18 +12,30 @@
 import SwiftUI
 
 struct ScrollSection: View {
+    
+    @State var title = "Now Playing"
+    
+    @State var posters: [String] = ["poster", "poster1", "poster2", "poster3", "poster4", "poster5", "poster6"]
+
     var body: some View {
 
         VStack {
-            Text("Now Playing")
+            Text(title)
                 .font(.system(size: 20, weight: .semibold, design: .rounded))
                 .foregroundColor(.white)
                 .padding(.horizontal, 20)
 
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20) {
-                    ForEach()
+                    ForEach(posters.indices, id: \.self) { index in
+                        Image(posters [index])
+                            .resizable()
+                            .frame(width: 100, height: 130)
+                            .cornerRadius(20)
+                    }
                 }
+                .offset(x: 20)
+                .padding(.trailing, 40)
             }
         }
     }
