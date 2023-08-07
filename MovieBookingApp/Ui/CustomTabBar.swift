53ab2e480905caef9f7b42e3ce9db6bf2f204b8d
Elaidzha1940 <eli080293@gmail.com>
2023-08-07 13:17:31 +0300

    added CustomTabBar / UI

---
+++ MovieBookingApp/Ui/CustomTabBar.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: MovieBookingApp
+//  File: CustomTabBar.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.08.2023
+//
+//  Status: In progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBar()
+    }
+}
