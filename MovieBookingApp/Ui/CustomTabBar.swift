088f229ba5887e40ee9b0f171fe7918b9fe5f590
Elaidzha1940 <eli080293@gmail.com>
2023-08-07 14:57:43 +0300

    CustomTabBar

--- MovieBookingApp/Ui/CustomTabBar.swift
+++ MovieBookingApp/Ui/CustomTabBar.swift
@@ -14,7 +14,10 @@ import SwiftUI
 struct CustomTabBar: View {
     
     @Binding var currentTab: Tab
+    
     var backgroundColors = [Color("some"), Color("blur"), Color("blur1")]
+    
+    var gradientCircle = [Color("some"), Color("some").opacity(0.1), Color("some")]
     
     var body: some View {
         
@@ -31,11 +34,26 @@ struct CustomTabBar: View {
                             .frame(maxWidth: .infinity)
                             .foregroundColor(.white)
                             .offset(y: currentTab == tab ? -15 : 0)
+                           
                     }
                 }
             }
             .frame(maxWidth: .infinity)
-            
+            .background(alignment: .leading) {
+                Circle()
+                    .fill(.ultraThinMaterial)
+                    .frame(width: 80, height: 80)
+                    .shadow(color: .black.opacity(0.25), radius: 20, x: 0, y: 0)
+                    .overlay(
+                    Circle()
+                        .trim(from: 0, to: CGFloat(0.5))
+                        .stroke(
+                            LinearGradient(colors: gradientCircle, startPoint: .top, endPoint: .bottom), style: StrokeStyle(lineWidth: 2)
+                        )
+                        .rotationEffect(.degrees(135))
+                        .frame(width: 80, height: 80)
+                    )
+            }
         }
         .frame(height: 25)
         .padding(.top, 30)
