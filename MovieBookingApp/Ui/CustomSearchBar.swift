9e7f5bfe00d9fea2d40247e4bbee0e54caf759ca
Elaidzha1940 <eli080293@gmail.com>
2023-08-07 15:53:44 +0300

    added CustomSearchBar

---
+++ MovieBookingApp/Ui/CustomSearchBar.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: MovieBookingApp
+//  File: CustomSearchBar.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.08.2023
+//
+//  Status: In progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct CustomSearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomSearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSearchBar()
+    }
+}
