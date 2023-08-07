46d99294a06c167fdf3c9d04e9d36d2f085fd429
Elaidzha1940 <eli080293@gmail.com>
2023-08-07 15:45:27 +0300

    added HomeView

---
+++ MovieBookingApp/Views/HomeView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: MovieBookingApp
+//  File: HomeView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.08.2023
+//
+//  Status: In progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
